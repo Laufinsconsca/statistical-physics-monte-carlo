4806ca57133809cc67015e0b24fd4f7a547ad5f8
Laufinsconsca <walle.log@yandex.ru>
2021-05-03 00:37:28 +0400

    Change the spacing of the argument of the paired correlation function

--- functions.py
+++ functions.py
@@ -14,7 +14,8 @@ def plot(x, f, title, x_label, y_label):
     :param y_label: обозначение шкалы ординат
     """
     plt.figure(figsize=(11, 8), dpi=80)
-    plt.scatter(x, f, s=5)
+    # plt.scatter(x, f, s=5) # построение графика в виде набора точек
+    plt.plot(x, f)  # построение сплошного графика
     plt.title(title)
     plt.xlabel(x_label)
     plt.ylabel(y_label)
