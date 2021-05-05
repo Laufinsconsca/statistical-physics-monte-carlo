df48cd0602140e2d93b01b697d9d203d2538230a
Laufinsconsca <walle.log@yandex.ru>
2021-05-06 01:28:22 +0400

    Add logging to CUDA implementation
    Signed-off-by: Laufinsconsca <walle.log@yandex.ru>

--- correlation_function_cuda.py
+++ correlation_function_cuda.py
@@ -1,13 +1,16 @@
 import numpy as np
 import pycuda.driver as drv
 from pycuda.autoinit import context
-import pycuda.autoinit
 from pycuda.compiler import SourceModule
+# noinspection PyUnresolvedReferences
+import pycuda.autoinit
 
 kernel_code = """
   #include <stdio.h>
+  #include "Windows.h"
 
-__global__ void kernel(float* molecules, int* N, double r, double delta_r, double L, int mdim, int ndim) {
+__global__ void kernel(float* molecules, int* N, double r, double delta_r, double L, int mdim, int ndim, 
+double* percentage_of_completion) {
   const int idx = 3 * (threadIdx.x + blockDim.x * blockIdx.x);
   const int idy = threadIdx.y + blockDim.y * blockIdx.y;
   if (idy < mdim && idx < ndim) {
@@ -51,6 +54,7 @@ __global__ void kernel(float* molecules, int* N, double r, double delta_r, doubl
         d = 0;
         } 
   }
+  atomicAdd(percentage_of_completion, 1);
   }
 }
 """
@@ -65,7 +69,7 @@ def calculate_pair_correlation_function(molecules_ensemble, r, delta_r, L, n, ex
     :param delta_r: толщина шарового слоя
     :param L: длина ребра ячейки моделирования
     :param n: средняя концентрация
-    :param execution_percentage: класс, хранящий параметры вывода процента выполнения в консол
+    :param execution_percentage: класс, хранящий параметры вывода процента выполнения в консоль
     :return: массив значений корреляционной функции
     """
     pair_corr_func = np.zeros(len(r))
@@ -78,19 +82,31 @@ def calculate_pair_correlation_function(molecules_ensemble, r, delta_r, L, n, ex
     calculate = mod.get_function("kernel")
     # --------------------------- </настраиваем ядро GPU> --------------------------------------------------------------
     delta_N = drv.managed_zeros(shape=1, dtype=np.int32, mem_flags=drv.mem_attach_flags.GLOBAL)
+    percentage_of_completion = drv.managed_zeros(shape=1, dtype=np.float64, mem_flags=drv.mem_attach_flags.GLOBAL)
+    h_p = 100 / len(r)
+    p = 1  # период отображения процента выполнения (в итерациях)
+    while execution_percentage.lower_bound > h_p * p:
+        p += 1
     # инициализируем delta_N в общей памяти
     for i in range(len(r)):
         calculate(drv.In(molecules_ensemble.flatten()),
                   delta_N,
                   r[i],
-                  np.float64(delta_r/2),
+                  np.float64(delta_r / 2),
                   np.float64(L),
                   np.int32(len(molecules_ensemble)),
                   np.int32(3 * len(molecules_ensemble[0])),
+                  percentage_of_completion,
                   block=block_dim, grid=grid_dim)
         context.synchronize()
         delta_N_temp = delta_N[0] / np.float64((len(molecules_ensemble) * len(molecules_ensemble[0])))
         # среднее количество частиц в шаровом слое
         pair_corr_func[i] = delta_N_temp / (4 * np.pi * r[i] * r[i] * delta_r * n)  # корреляционная функция
         delta_N[0] = 0
+        if execution_percentage.output_percentage_to_console and i % p == 0:
+            percentage_of_completion_str = "Расчёт корреляционной функции: {:." \
+                                           + str(execution_percentage.number_of_decimal_places) + "f}%"
+            print(percentage_of_completion_str
+                  .format(100 * percentage_of_completion[0] / (len(r) * len(molecules_ensemble)
+                                                               * len(molecules_ensemble[0]))))
     return pair_corr_func
